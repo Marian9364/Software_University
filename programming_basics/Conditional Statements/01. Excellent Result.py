c64e545343a13de66248203b50dc57800bb6f3d9
Marian9364 <71733160+Marian9364@users.noreply.github.com>
2020-12-09 20:29:21 +0100

    Create 01. Excellent Result.py

---
+++ programming_basics/Conditional Statements/01. Excellent Result.py
@@ -0,0 +1,6 @@
+#Program that takes input from an user and prints output only if the given output is a number higher than 5.50
+
+grade = float(input())
+
+if grade >= 5.50:
+    print("Excellent!")
