b0539e3cb574b5633d2457e85277ab0df269bbb4
Marian9364 <71733160+Marian9364@users.noreply.github.com>
2020-12-11 20:24:37 +0100

    Create 05. Number in Range.py

---
+++ programming_basics/Conditional Statements Advanced/05. Number in Range.py
@@ -0,0 +1,8 @@
+# checking if a number is between 0 and 100; practicsing the 'and' statement
+
+num = int(input())
+
+if num >= -100 and num <= 100 and num != 0:
+    print("Yes")
+else:
+    print("No")
