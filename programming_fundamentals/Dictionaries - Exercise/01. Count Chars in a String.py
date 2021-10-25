2d8117349128c91eb4894a659347fdb4f9866306
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-25 10:42:55 +0200

    Update 01. Count Chars in a String.py

--- programming_fundamentals/Dictionaries - Exercise/01. Count Chars in a String.py
+++ programming_fundamentals/Dictionaries - Exercise/01. Count Chars in a String.py
@@ -1,1 +1,12 @@
+text = input()
+my_dict = {}
+for letter in text:
+    if letter == " ":
+        continue
+    if letter not in my_dict:
+        my_dict[letter] = 1
+    else:
+        my_dict[letter] += 1
 
+for key, value in my_dict.items():
+    print(f"{key} -> {value}")
