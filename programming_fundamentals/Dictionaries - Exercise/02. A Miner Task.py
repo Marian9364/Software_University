ca56d355e77d64d4db28f3bf6d9bc31a80b4a695
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-25 10:43:19 +0200

    Update 02. A Miner Task.py

--- programming_fundamentals/Dictionaries - Exercise/02. A Miner Task.py
+++ programming_fundamentals/Dictionaries - Exercise/02. A Miner Task.py
@@ -1,1 +1,14 @@
+command = input()
+database = {}
+while command != "stop":
+    resource = command
+    amount = int(input())
+    if resource not in database:
+        database[resource] = amount
+    else:
+        database[resource] += amount
 
+    command = input()
+
+for key, value in database.items():
+    print(f"{key} -> {value}")
