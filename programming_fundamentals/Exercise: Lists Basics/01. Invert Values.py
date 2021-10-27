20475dc8d3b7510133c088ba2f11a9c993075e7a
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-27 17:34:12 +0200

    Update 01. Invert Values.py

--- programming_fundamentals/Exercise: Lists Basics/01. Invert Values.py
+++ programming_fundamentals/Exercise: Lists Basics/01. Invert Values.py
@@ -1,1 +1,10 @@
+single_string = input().split()
+new_list = []
+for num in single_string:
+    if int(num) > 0:
+        num = -1 * int(num)
+    elif int(num) <= 0:
+        num = abs(int((num)))
+    new_list.append(num)
 
+print(new_list)
