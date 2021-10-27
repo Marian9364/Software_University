d965fa10c1aebaf4e93a48311da2ba9874462ce2
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-27 17:36:38 +0200

    Update 06. Survival of the Biggest.py

--- programming_fundamentals/Exercise: Lists Basics/06. Survival of the Biggest.py
+++ programming_fundamentals/Exercise: Lists Basics/06. Survival of the Biggest.py
@@ -1,1 +1,14 @@
+list_of_ints = input().split()
+n = int(input())
+#print(list_of_ints)
+new_int_list = []
+for i in list_of_ints:
+    new_int_list.append(int(i))
+#print(new_int_list)
+for i in range(n):
 
+    b = min(new_int_list)
+    new_int_list.remove(b)
+
+
+print(new_int_list)
