68750cab8844c05d19251dcfbac65f8a4dcfb374
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-27 17:34:59 +0200

    Update 02. Multiples List.py

--- programming_fundamentals/Exercise: Lists Basics/02. Multiples List.py
+++ programming_fundamentals/Exercise: Lists Basics/02. Multiples List.py
@@ -1,1 +1,10 @@
+factor_and_count = int(input())
+length = int(input())
 
+my_list = []
+
+for num in range(1,length + 1):
+    new_number =  factor_and_count * num
+    my_list.append(new_number)
+
+print(my_list)
