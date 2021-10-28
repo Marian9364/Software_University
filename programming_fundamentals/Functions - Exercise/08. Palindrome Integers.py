0e4951e3474be004fe3b23806c88870bf1df0c89
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-28 18:10:58 +0200

    Update 08. Palindrome Integers.py

--- programming_fundamentals/Functions - Exercise/08. Palindrome Integers.py
+++ programming_fundamentals/Functions - Exercise/08. Palindrome Integers.py
@@ -1,1 +1,11 @@
+def palindromes(inputing_list):
+    for single_item in inputing_list:
+        if single_item == single_item[::-1]:
+            print("True")
+        else:
+            print("False")
 
+
+string_list = input().split(", ")
+
+palindromes(string_list)
