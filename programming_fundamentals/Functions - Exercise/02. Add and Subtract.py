acbf91441766ff3039587ee0b5d5924961e3b5e2
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-28 18:09:58 +0200

    Update 02. Add and Subtract.py

--- programming_fundamentals/Functions - Exercise/02. Add and Subtract.py
+++ programming_fundamentals/Functions - Exercise/02. Add and Subtract.py
@@ -1,1 +1,18 @@
+def sum_numbers(int1, int2,):
+    sum_ints = int1 + int2
+    return sum_ints
 
+def subtract(int1, int2, num3):
+    subtracted_sum = None
+    subtracted_sum = sum_numbers(int1, int2) - num3
+    return subtracted_sum
+
+def add_and_subtract(int1, int2, num3):
+    sum_numbers(int1, int2)
+    subtract(int1, int2, num3)
+
+    final_sum = subtract(int1, int2, num3)
+    return final_sum
+
+
+print(add_and_subtract(int(input()), int(input()), int(input())))
