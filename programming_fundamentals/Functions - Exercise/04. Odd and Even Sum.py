97b063590057c2799d4c82381ac2162a21c56fad
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-28 18:10:34 +0200

    Update 04. Odd and Even Sum.py

--- programming_fundamentals/Functions - Exercise/04. Odd and Even Sum.py
+++ programming_fundamentals/Functions - Exercise/04. Odd and Even Sum.py
@@ -1,1 +1,13 @@
+def odd_even_sum(n):
 
+    odd_sum = 0
+    even_sum = 0
+    for i in n:
+        if int(i) % 2 == 1:
+            odd_sum += int(i)
+        elif int(i) % 2 == 0:
+            even_sum += int(i)
+
+    print(f"Odd sum = {odd_sum}, Even sum = {even_sum}")
+num = input()
+odd_even_sum(num)
