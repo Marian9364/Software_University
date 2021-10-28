ec7bb855b7b3758603128bbd40e75049e771f37f
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-28 18:11:11 +0200

    Update 09. Password Validator.py

--- programming_fundamentals/Functions - Exercise/09. Password Validator.py
+++ programming_fundamentals/Functions - Exercise/09. Password Validator.py
@@ -1,1 +1,20 @@
+def password_validator(password):
+    nums = 0
+    is_valid = True
+    if len(password) < 6 or len(password) > 10:
+        print("Password must be between 6 and 10 characters")
+        is_valid = False
+    if not password.isalnum():
+        print("Password must consist only of letters and digits")
+        is_valid = False
+    for num in password:
+        if num.isdigit():
+            nums += 1
+    if nums < 2:
+        print("Password must have at least 2 digits")
+        is_valid = False
 
+    if is_valid:
+        print("Password is valid")
+
+password_validator(input())
