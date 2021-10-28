741275415440e86f180368cf5d046e3c3b354feb
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-28 18:10:17 +0200

    Update 03. Characters in Range.py

--- programming_fundamentals/Functions - Exercise/03. Characters in Range.py
+++ programming_fundamentals/Functions - Exercise/03. Characters in Range.py
@@ -1,1 +1,7 @@
+def ascii_inbetweener(sign1, sign2):
+    sign1 = ord(sign1)
+    sign2 = ord(sign2)
+    for i in range(sign1 +1, sign2):
+        print(chr(i), end=" ")
 
+ascii_inbetweener(input(), input())
