3b63ae035fd154b4cfbccaf78a1dc7fdb3402083
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-10-28 18:11:28 +0200

    Update 10. Perfect Number.py

--- programming_fundamentals/Functions - Exercise/10. Perfect Number.py
+++ programming_fundamentals/Functions - Exercise/10. Perfect Number.py
@@ -1,1 +1,13 @@
+def perfect_number(num):
+    num = int(num)
+    divisors = 0
+    for i in range(1, num):
+        if num % i == 0:
+            divisors += i
+    if divisors == num:
+        print("We have a perfect number!")
+    else:
+        print("It's not so perfect.")
 
+
+perfect_number(int(input()))
