912a6c97a008b6bae4d485eaa51444b68d2a8d84
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-07 22:55:10 +0100

    Update 02. Next Version.py

--- programming_fundamentals/Lists Advanced - Exercise/02. Next Version.py
+++ programming_fundamentals/Lists Advanced - Exercise/02. Next Version.py
@@ -1,1 +1,10 @@
+version = input().split(".")
+new_list = "".join(version)
+new_list = int(new_list)
+next_version = new_list + 1
+next_version = str(next_version)
+next_version_list = []
+for num in next_version:
+    next_version_list.append(num)
 
+print(".".join(next_version_list))
