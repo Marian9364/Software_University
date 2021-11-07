9d3dc385fc6c8f29bf604b8fa4c1cf5d72cefa60
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-07 22:55:52 +0100

    Update 07. Group of 10's.py

--- programming_fundamentals/Lists Advanced - Exercise/07. Group of 10's.py
+++ programming_fundamentals/Lists Advanced - Exercise/07. Group of 10's.py
@@ -1,1 +1,16 @@
+numbers = input().split(", ")
+integers = [int(el) for el in numbers]
+boundary = 10
+for i in range(max(integers)):
+    if len(numbers) == 0:
+        break
+    result_list = []
+    for num in numbers:
+        num = int(num)
+        if num <= boundary:
+            result_list.append(num)
+    for result in result_list:
+        numbers.remove(str(result))
 
+    print(f"Group of {boundary}'s: {result_list}")
+    boundary += 10
