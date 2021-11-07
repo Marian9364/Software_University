cf8c5e466a3bf37a7bbcfe6ab4a543506acf3cc2
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-07 22:54:44 +0100

    Update 01. Which Are In?.py

--- programming_fundamentals/Lists Advanced - Exercise/01. Which Are In?.py
+++ programming_fundamentals/Lists Advanced - Exercise/01. Which Are In?.py
@@ -1,1 +1,10 @@
+list_1 = input().split(", ")
+list_2 = input().split(", ")
+result_list = []
+for word in list_1:
+    for words in list_2:
+        if word in words:
+            if word not in result_list:
+                result_list.append(word)
 
+print(result_list)
