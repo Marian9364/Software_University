e59fe79bd84ded2599b1b4ba3d587de4f4409d7d
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-08 22:35:38 +0100

    Update 05. Numbers Filter.py

--- programming_fundamentals/Lists Basics/05. Numbers Filter.py
+++ programming_fundamentals/Lists Basics/05. Numbers Filter.py
@@ -1,1 +1,26 @@
+n = int(input())
 
+even = []
+odd = []
+negative = []
+positive = []
+
+for i in range(n):
+    nums = int(input())
+    if nums % 2 == 0:
+        even.append(nums)
+    if nums % 2 == 1:
+        odd.append(nums)
+    if nums >= 0:
+        positive.append(nums)
+    if nums < 0:
+        negative.append(nums)
+command = input()
+if command == "even":
+    print(even)
+elif command == "odd":
+    print(odd)
+elif command == "negative":
+    print(negative)
+elif command == "positive":
+    print(positive)
