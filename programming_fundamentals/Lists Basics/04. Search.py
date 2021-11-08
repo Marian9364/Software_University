1d36adb4e2038a0cd1d15f536155f69d6a87dad4
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-08 22:35:18 +0100

    Update 04. Search.py

--- programming_fundamentals/Lists Basics/04. Search.py
+++ programming_fundamentals/Lists Basics/04. Search.py
@@ -1,1 +1,13 @@
+n = int(input())
+full_list = []
+reworked_list = []
+word = input()
+for i in range(n):
+    strings = input()
+    full_list.append(strings)
+for chars in full_list:
+    if word in chars:
+        reworked_list.append(chars)
 
+print(full_list)
+print(reworked_list)
