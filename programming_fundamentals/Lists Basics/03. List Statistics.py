e36704ffb7d1dc5af2442d1493d581fbff345201
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-08 22:35:00 +0100

    Update 03. List Statistics.py

--- programming_fundamentals/Lists Basics/03. List Statistics.py
+++ programming_fundamentals/Lists Basics/03. List Statistics.py
@@ -1,1 +1,14 @@
+n = int(input())
 
+positive = []
+negative = []
+for i in range(n):
+    num = int(input())
+    if num >= 0:
+        positive.append(num)
+    else:
+        negative.append(num)
+print(positive)
+print(negative)
+
+print(f"Count of positives: {len(positive)}. Sum of negatives: {sum(negative)}")
