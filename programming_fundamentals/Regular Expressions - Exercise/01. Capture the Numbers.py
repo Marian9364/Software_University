0118c12432c1479f3ae49285988155d132c4d420
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-09 17:29:49 +0100

    Update 01. Capture the Numbers.py

--- programming_fundamentals/Regular Expressions - Exercise/01. Capture the Numbers.py
+++ programming_fundamentals/Regular Expressions - Exercise/01. Capture the Numbers.py
@@ -1,1 +1,11 @@
+import re
+pattern = r"\d+"
+matches_filled = []
+text = input()
+while text:
+    matches = re.findall(pattern, text)
+    matches_filled.extend(matches)
 
+    text = input()
+
+print(*matches_filled, sep=" ")
