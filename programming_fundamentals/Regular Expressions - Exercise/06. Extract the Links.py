cf4c1e309f81ec1a82a3598dc69eb8530bf58654
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2021-11-09 17:31:18 +0100

    Update 06. Extract the Links.py

--- programming_fundamentals/Regular Expressions - Exercise/06. Extract the Links.py
+++ programming_fundamentals/Regular Expressions - Exercise/06. Extract the Links.py
@@ -1,1 +1,9 @@
-
+import re
+pattern = r"(www.[a-zA-Z0-9\-]+(\.[a-z]+){1,})"
+ll = []
+data = input()
+while len(data) > 0:
+    matches = re.finditer(pattern, data)
+    for match in matches:
+        print(match.group())
+    data = input()
