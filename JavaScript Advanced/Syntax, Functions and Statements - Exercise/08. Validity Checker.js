974e07a4f64e35a569cd426e930a1855d5818f08
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-01-15 17:13:11 +0100

    Create 08. Validity Checker.js

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Exercise/08. Validity Checker.js
@@ -0,0 +1,30 @@
+function checkNumbers(X1, Y1, X2, Y2) {
+ 
+    let x1 = Number(X1);
+    let y1 = Number(Y1);
+ 
+    let x2 = Number(X2);
+    let y2 = Number(Y2);
+ 
+    let formula = function(x1, y1, x2, y2) {
+        let num = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        let result = '';
+        if (Math.round(num) == num) {
+            result = 'valid'
+ 
+        }
+        else{
+            result = 'invalid';
+        }
+        return result;
+    }
+ 
+    let result1 = formula(x1, y1, 0, 0);
+    let result2 = formula(x2, y2, 0, 0);
+    let result3 = formula(x1, y1, x2, y2);
+ 
+    console.log('{' + x1 + ', ' + y1 + '} to {0, 0} is ' + result1 );
+    console.log('{' + x2 + ', ' + y2 + '} to {0, 0} is ' + result2 );
+    console.log('{' + x1 + ', ' + y1 + '} to ' + '{' + x2 + ', ' + y2 + '} is ' + result3);
+}
+
