0386b3346dfbd80956a4a18a3822b4510a545f81
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-01-15 17:05:56 +0100

    Create 01. Fruit.js

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Exercise/01. Fruit.js
@@ -0,0 +1,6 @@
+function calcSum(fruit, weightInGrams, pricePerKg) {
+    let weightInKg = weightInGrams / 1000
+    let sum = weightInKg * pricePerKg
+    console.log(`I need $${sum.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`);
+
+}
