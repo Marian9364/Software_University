096f1fbd71e28c79293607886b81e54db340e11e
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-01-18 10:02:23 +0100

    Create 06. List Of Names.js

---
+++ JavaScript Advanced/Arrays and Nested Arrays - Exercise/06. List Of Names.js
@@ -0,0 +1,6 @@
+function solve(input) {
+    let sorted = input.sort((a,b) => a.localeCompare(b));
+    for(let i = 0; i < sorted.length ; i++) {
+        console.log(`${i+1}.${sorted[i]}`);
+    }
+}
