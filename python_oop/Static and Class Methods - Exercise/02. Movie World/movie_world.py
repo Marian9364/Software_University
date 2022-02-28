3f4f6bd818f142a9ade05c516cef3d6bf3e76fc5
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-02-28 13:21:56 +0100

    Add files via upload

---
+++ python_oop/Static and Class Methods - Exercise/02. Movie World/movie_world.py
@@ -0,0 +1,67 @@
+from project.customer import Customer
+from project.dvd import DVD
+
+
+class MovieWorld:
+    def __init__(self, name):
+        self.name = name
+        self.customers = []
+        self.dvds = []
+
+    @staticmethod
+    def dvd_capacity():
+        return 15
+
+    @staticmethod
+    def customer_capacity():
+        return 10
+
+    def add_customer(self, customer: Customer):
+        if len(self.customers) < MovieWorld.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD):
+        if len(self.dvds) < MovieWorld.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id, dvd_id):
+        customer = self.__get_obj_by_id(self.customers, customer_id)
+        dvd = self.__get_obj_by_id(self.dvds, dvd_id)
+
+        if dvd in customer.rented_dvds:
+            return f"{customer.name} has already rented {dvd.name}"
+
+        if dvd.is_rented:
+            return "DVD is already rented"
+
+        if dvd.age_restriction > customer.age:
+            return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
+
+        dvd.is_rented = True
+        customer.rented_dvds.append(dvd)
+        return f"{customer.name} has successfully rented {dvd.name}"
+
+    def return_dvd(self, customer_id, dvd_id):
+        customer = self.__get_obj_by_id(self.customers, customer_id)
+        dvd = self.__get_obj_by_id(self.dvds, dvd_id)
+
+        if dvd not in customer.rented_dvds:
+            return f"{customer.name} does not have that DVD"
+
+        dvd.is_rented = False
+        customer.rented_dvds.remove(dvd)
+        return f"{customer.name} has successfully returned {dvd.name}"
+
+
+
+    def __repr__(self):
+        result = "\n".join([str(x) for x in self.customers])
+        result += "\n"
+        result += "\n".join([str(x) for x in self.dvds])
+
+        return result
+
+    def __get_obj_by_id(self, objects, id):
+        for object in objects:
+            if object.id == id:
+                return object
